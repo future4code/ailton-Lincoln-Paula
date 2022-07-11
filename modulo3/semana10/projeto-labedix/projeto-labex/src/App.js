431ca17867c41c3b542e9f6e9cb960f9ff28d4e9
Lincoln Ribeiro de Paula <lincoln20012@outlook.com>
2022-07-11 17:03:52 -0300

    Projetando01

---
+++ modulo3/semana10/projeto-labedix/projeto-labex/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+const Text = styled.p`
+
+font-family: 'Roboto', sans-serif;
+
+`
+
+function App() {
+  return (
+    <div>oi
+    </div>
+  )
+}
+
+export default App
