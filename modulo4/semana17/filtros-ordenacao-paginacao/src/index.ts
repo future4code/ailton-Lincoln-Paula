2df50de9a9d827d91edb25ade6ebecb51314fd92
Lincoln Ribeiro de Paula <lincoln20012@outlook.com>
2022-08-30 22:03:14 -0300

    atv-1&&2

--- modulo4/semana17/filtros-ordenacao-paginacao/src/index.ts
+++ modulo4/semana17/filtros-ordenacao-paginacao/src/index.ts
@@ -1,5 +1,9 @@
 import { app } from "./app";
-import { getAllRecipes } from "./endpoints/getAllRecipes";
+import { getAllUsers } from "./endpoints/exercicio1/getAllUsers";
+import {getUserType} from './endpoints/exercicio1/getUserType'
+import {getUserOrder} from './endpoints/exercicio2/getUserOrder'
 
-app.get("/recipes",getAllRecipes)
+app.get("/user", getAllUsers)
+app.get("/user/:type", getUserType)
+app.get("/user/:name/:order/:type", getUserOrder)
 
